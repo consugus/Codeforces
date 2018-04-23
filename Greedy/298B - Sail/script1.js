92811e3d2514c60624a9d68db994fd9a85a9714f
Gustavo <consugus@gmail.com>
2018-04-23 17:58:38 -0300

    Some advance

--- Greedy/298B - Sail/script1.js
+++ Greedy/298B - Sail/script1.js
@@ -3,7 +3,7 @@
 //  * -10^9 <= direction < 10^9
 
 //Input
-var t = "5 0 0 1 1"; var direction = "SESNW";           // 4
+var firstLine = "5 0 0 1 1"; var direction = "SESNW";           // 4
 // var t = "10 5 3 3 6"; var direction = "NENSWESNEE";     // -1
 // var t = "19 172106364 468680119 172106365 468680119"; var direction = "SSEEESSSESESWSEESSS";    // 13
 
@@ -12,16 +12,26 @@ var t = "5 0 0 1 1"; var direction = "SESNW";           // 4
 
 
 
-retorno = eval(t, direction);
+retorno = eval(firstLine, direction);
 
 //Solution
-function eval(t, direction){
-    t = t.split(" ").map(function(x){ return parseInt(x)});
-    console.log(timeToReach(t, direction));
+function eval(firstLine, direction){
+    var tmp = firstLine.split(" ").map(function(x){ return parseInt(x)});
+    var t = tmp[0];
+    var start = {
+        x: tmp[1],
+        y: tmp[2]
+    };
+    var end = {
+        x: tmp[3],
+        y: tmp[4]
+    };
+    console.log(timeToReach(t, start, end, direction));
 };
 
-function timeToReach(t, direction){
+function timeToReach(t, s, e, direction){
     var answer = -1;
+    console.log("s.x: " + s.x + "    s.y: " + s.y + "        e.x: " + e.x + "    e.y: " + e.y);
 
     return answer;
 };

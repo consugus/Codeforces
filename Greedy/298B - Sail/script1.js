0a02642dd5dc5b2bb0b37045b154019dee1a740e
Gustavo <consugus@gmail.com>
2018-04-23 21:37:56 -0300

    298B - Sail - Solved

--- Greedy/298B - Sail/script1.js
+++ Greedy/298B - Sail/script1.js
@@ -3,14 +3,13 @@
 //  * -10^9 <= direction < 10^9
 
 //Input
-var firstLine = "5 0 0 1 1"; var direction = "SESNW";           // 4
-// var t = "10 5 3 3 6"; var direction = "NENSWESNEE";     // -1
-// var t = "19 172106364 468680119 172106365 468680119"; var direction = "SSEEESSSESESWSEESSS";    // 13
+// var firstLine = "5 0 0 1 1"; var direction = "SESNW";           // 4
+// var firstLine = "10 5 3 3 6"; var direction = "NENSWESNEE";     // -1
+// var firstLine = "19 172106364 468680119 172106365 468680119"; var direction = "SSEEESSSESESWSEESSS";
+//              13
+// var firstLine = "19 172106364 468680119 172106363 468680119"; var direction = "SSEEESSSESESWSEESSS";
 
-// var t = "10 5 3 3 6"; var direction = "NENSWESNEE";
-// var t = "10 5 3 3 6"; var direction = "NENSWESNEE";
-
-
+var firstLine = readline(); var direction = readline();
 
 retorno = eval(firstLine, direction);
 
@@ -20,20 +19,52 @@ function eval(firstLine, direction){
     var t = tmp[0];
     var start = {
         x: tmp[1],
-        y: tmp[2]
-    };
+        y: tmp[2]    };
     var end = {
         x: tmp[3],
-        y: tmp[4]
-    };
-    console.log(timeToReach(t, start, end, direction));
+        y: tmp[4]    };
+    write( timeToReach(t, start, end, direction) );
+    //console.log(timeToReach(t, start, end, direction));
 };
 
 function timeToReach(t, s, e, direction){
-    var answer = -1;
-    console.log("s.x: " + s.x + "    s.y: " + s.y + "        e.x: " + e.x + "    e.y: " + e.y);
+    var ap = {
+        x: s.x,
+        y: s.y};
 
-    return answer;
+    var totalCount = -1;
+    for(var i = 0 ; i < t ; i++){
+        switch(direction[i]){
+            case "N": // north direction
+                if( Math.abs(e.y - (ap.y + 1)) < Math.abs(e.y - ap.y) ){
+                    ap.y ++;
+                };
+                break;
+
+            case "S": //south direction
+                if( Math.abs(e.y - (ap.y - 1)) < Math.abs(e.y - ap.y) ){
+                    ap.y --;
+                };
+                break;
+
+            case "E": // east direction
+                if( Math.abs(e.x - (ap.x + 1)) < Math.abs(e.x - ap.x) ){
+                    ap.x++;
+                };
+                break;
+
+            case "W": //west direction
+                if( Math.abs(e.x - (ap.x - 1)) < Math.abs(e.x - ap.x) ){
+                    ap.x--;
+                };
+                break;
+        };
+        if( (e.x == ap.x) && (e.y == ap.y) ){
+            totalCount = i + 1;
+            break;
+        };
+    };
+    return totalCount;
 };
 
 

1056ca088d39afe550ba3add006728ead318d8ae
Gustavo <consugus@gmail.com>
2018-04-26 14:04:45 -0300

    Fuck!

--- 05 - Dinamic Programing/A - Cut a Ribbon/script1.js
+++ 05 - Dinamic Programing/A - Cut a Ribbon/script1.js
@@ -12,9 +12,10 @@ var firstLine = "5 5 3 2" // 2
 // var firstLine = "100 23 15 50" // 2
 // var firstLine = "3119 3515 1021 7" // 11
 // var firstLine = "918 102 1327 1733" // 9
-//              var firstLine = "3164 42 430 130*" // 15
+            //  var firstLine = "3164 42 430 130*" // 15
 // var firstLine = "370 2 1 15" // 370
 // var firstLine = "418 18 14 17" // 29
+// var firstLine = "5 5 6 7";
 // endregion
 
 retorno = eval(firstLine);
@@ -28,22 +29,27 @@ function eval(firstLine){
     console.log("tmp: " + nabc + "    n: " + n + "    a: " + a + "    b: " + b + "    c: " + c + "\n\n");
 
     // write(getMaximumCuts(n, a, b, c));
-    console.log(getMax(n));
+    //console.log(getMax(n));
 
 
-    function getMax(n){
+    function f(n){
+        //console.log(n);
         if(n in results){ return results[n];};
         if(n == 0){ return 0;};
         if(n < Math.min(a, b, c)){ return -n;};
 
-        console.log(    "getMax(" + n + " - " + a + "): " + getMax(n - a) , "    getMax(" + n + " - " + b + "): " + getMax(n - b) , "    getMax(" + n + " - " + c + "): " + (getMax(n - c) + 1));
-        var tmp = Math.max(   getMax(n - a) , getMax(n - b) , getMax(n - c) + 1   );
+        console.log(    "getMax(" + n + " - " + a + "): " + f(n - a) , "    getMax(" + n + " - " + b + "): " + f(n - b) , "    getMax(" + n + " - " + c + ") + 1: " + (f(n - c) + 1  ));
+        var tmp = Math.max(   f(n - a), f(n - b), f(n - c) +1) ;
 
-        console.log("tmp: " + tmp + "    results: " + results);
         results[n] = tmp;
+        console.log("tmp: " + tmp + "    results: " + results + "    len: " + results.length + "\n\n");
 
-        return results[n];
+        return tmp;
     };
+
+
+    console.log(f(n));
+
 };
 
 
